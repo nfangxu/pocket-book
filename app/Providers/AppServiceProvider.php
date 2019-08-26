47c28ed700b7ca768368af718487f9289b7e2bd4
nfangxu <nfangxu@gmail.com>
2019-08-26 16:09:48 +0800

    update laravel to 5.8.* and add web-tinker

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Gate::define('viewWebTinker', function ($user = null) {
+            return $user && in_array($user->email, [
+                "nfangxu@gmail.com",
+            ]);
+        });
     }
 }
